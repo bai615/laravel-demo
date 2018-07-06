38f195b0132021c0aefda77e661eeaf98f63553b
baihua@sndskj.com <baihua2018>
2018-07-06 14:38:58 +0800

    创建数据表
    # Conflicts:
    #	composer.json

---
+++ database/migrations/2018_07_06_062457_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id')->comment('商品ID');
+            $table->string('name',100)->comment('商品名称');
+            $table->string('summary',2000)->comment('商品简介');
+            $table->text('detail')->comment('商品详情');
+            $table->integer('status')->comment('状态');
+            $table->integer('sort')->comment('排序序号');
+            $table->string('remark',500)->comment('备注');
+//            $table->dateTime('created_at')->comment('创建时间'); // 系统默认添加此字段，类型：timestamp
+//            $table->dateTime('updated_at')->nullable()->comment('最后更新时间'); // 系统默认添加此字段，类型：timestamp
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
