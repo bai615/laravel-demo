92c482ac7180276ac0cfbcad9e3e71e7678164d2
baihua@sndskj.com <baihua2018>
2018-07-16 16:25:54 +0800

    GraphQL 测试

---
+++ app/GraphQL/Type/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\GraphQL\Type;
+
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Type as BaseType;
+use GraphQL;
+
+class UserType extends BaseType
+{
+    protected $attributes = [
+        'name' => 'User',
+        'description' => 'A User type'
+    ];
+
+    public function fields()
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The email of the user'
+            ],
+            'email' => [
+                'type' => Type::string(),
+                'description' => 'The email of the user'
+            ]
+        ];
+    }
+
+    protected function resolveEmailField($root, $args)
+    {
+        return strtolower($root->email);
+    }
+}
